b1f9561b5438f5f4cb06c7a3aa7c2fe2a5da462a
vidyasagar12344 <vidyasagar669@gmail.com>
2019-01-04 19:23:18 +0530

    * Changed Cardys Services * Modified leave management component

--- src/app/LeaveManagement/RaiseLeave/raise-leave.ts
+++ src/app/LeaveManagement/RaiseLeave/raise-leave.ts
@@ -1,24 +1,65 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit,Inject} from '@angular/core';
 import {Router} from '@angular/router';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { SOAPHandlerService } from '../../soap-handler.service'
+declare var $:any;
 @Component({
     selector: 'raise-leave',
     templateUrl: './raise-leave.html',
   
   })
+ 
   export class leaveRequestComponent implements OnInit{
     leaveShow:boolean;
-    
+    user_id:any;
+    remaining_leaves:any;
+    applied_leaves:any;
+    startDate:any;
+    endDate:any;
+    reasonLeave:any;
+    noOfLeaves:any;
     ngOnInit(): void {
       let i=localStorage.getItem("LeaveRequest");
-      //let j=localStorage.getItem("InitialValue");
+   
       if(i=='1'){
         this.leaveShow=true;
-        //localStorage.removeItem("LeaveRequest");
+      
       }
     }
-    constructor(private router:Router){
+    constructor(private router:Router,public dialogRef: MatDialogRef<leaveRequestComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: any,private soapHandler:SOAPHandlerService){
+         this.user_id=data.userID;
+         soapHandler.UserDetailsFromCordys(this.user_id).subscribe(
+           (response) =>{
+            let tupleNodes = $.cordys.json.findObjects(response, 'USER_DETAILS');
+            console.log(tupleNodes);
+            this.remaining_leaves=tupleNodes[0].NO_OF_LEAVES_REMAIN;
+            this.applied_leaves=tupleNodes[0].NO_OF_LEAVES_APPLIED;
+            console.log("the applied leaves is"+this.applied_leaves);
+            
+           }
+         )
+
 
     }
+
+    public setLeavesreqIntoDB(){
+      const paramObj={
+        userID:this.user_id,
+        start_date:this.startDate,
+        end_date:this.endDate,
+        reason_leave:this.reasonLeave,
+        no_of_leaves:this.noOfLeaves
+      };
+      this.soapHandler.saveLeaveReqIntoDB(paramObj).subscribe(
+        (response) =>{
+          console.log("Successfully Inserted"+response);
+
+          this.dialogRef.close();
+        }
+      )
+    }
+    
 
     
   }
--- src/app/LeaveManagement/leave-management.ts
+++ src/app/LeaveManagement/leave-management.ts
@@ -3,6 +3,8 @@ import {Router} from '@angular/router';
 import {MatIconModule,MatSidenavModule} from '@angular/material';
 import { SOAPHandlerService } from '../soap-handler.service'
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {leaveRequestComponent} from './RaiseLeave/raise-leave'
 
 export interface Tile {
   color: string;
@@ -21,18 +23,29 @@ declare var $:any;
     username:any;
     gender:any;
     manager:any;
-    leavesCount:any;
+    leaves_applied:any;
+    location:any;
     phoneNo:any;
+    mode:any;
+    designation:any;
+    experience:any;
+    father_name:any;
+    mother_name:any;
+    projects:any;
+    emergency_contact:any;
+    status:any;
+    leaves_remain:any;
+    user_id:any;
    
     ngOnInit(): void {
-     // this.userDetailsTemplates();
+      this.userDetailsTemplates();
       //this.loginHide=true;
       let leaveComp= localStorage.getItem("InitialValue");
        if(leaveComp=="AppCompShow"){
         this.leaveHide=true;
        }
     }
-   constructor(private sOAPHandlerService:SOAPHandlerService,private router:Router){
+   constructor(private sOAPHandlerService:SOAPHandlerService,private router:Router,private dialog:MatDialog){
 
    }
    public userDetailsTemplates(){
@@ -40,12 +53,23 @@ declare var $:any;
      (response:any) =>{
        let tupleNodes = $.cordys.json.findObjects(response, 'USER_DETAILS');
        console.log(tupleNodes);
+       this.user_id=tupleNodes[0].USER_ID;
        this.username=tupleNodes[0].USER_NAME;
        console.log("the username is"+this.username);
        this.gender=tupleNodes[0].GENDER;
        this.manager=tupleNodes[0].MANAGER;
-       this.leavesCount=tupleNodes[0].NO_OF_LEAVES_REMAIN;
+       this.leaves_remain=tupleNodes[0].NO_OF_LEAVES_REMAIN;
        this.phoneNo=tupleNodes[0].PHONE_NO;
+       this.location=tupleNodes[0].LOCATION;
+       this.mode=tupleNodes[0].PERIOD;
+       this.designation=tupleNodes[0].DESIGNATION;
+       this.experience=tupleNodes[0].EXPERIENCE;
+       this.projects=tupleNodes[0].PROJECTS;
+       this.father_name=tupleNodes[0].FATHER_NAME;
+       this.mother_name=tupleNodes[0].MOTHER_NAME;
+       this.emergency_contact=tupleNodes[0].EMERGENCY_CONTACT;
+       this.status=tupleNodes[0].STATUS;
+       this.leaves_applied=tupleNodes[0].NO_OF_LEAVES_APPLIED;
      },
      (err)=>{
 
@@ -60,12 +84,19 @@ declare var $:any;
      )
     }
     public openLeaveReq(){
-      localStorage.setItem("LeaveRequest","1");
+     
       this.router.navigate(['/leave']);
     }
     public onLogoutClick(){
       localStorage.removeItem("InitialValue");
       this.router.navigate(['/login']);
   
+    }
+    public leaveRequestRise(){
+      localStorage.setItem("LeaveRequest","1");
+        const dialogRef= this.dialog.open(leaveRequestComponent,{
+          width: '800px',
+          data: {userID:this.user_id}
+         });
     }
   }
